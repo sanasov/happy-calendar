506a89a9261d33899416f2c8ac205f505d527bc2
sanasov <igrey@mail.ru>
2017-08-28 20:14:03 +0300

    add scss

--- hp/webpack.config.js
+++ hp/webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-
 const SRC_DIR = path.resolve(__dirname, 'src/main/client');
 const DEST_DIR = path.resolve(__dirname, 'src/main/webapp/resources/app');
 const NODE_MODULE_ROOT = path.resolve(__dirname, 'node_modules');
@@ -14,7 +13,7 @@ function isExternal(module) {
 
 module.exports = {
 
-    devtool: 'cheap-module-eval-source-map', // for view src in browser
+    devtool: 'inline-cheap-module-source-map', // for view src in browser
     watch: false,
     context: SRC_DIR,
     entry: {
@@ -25,7 +24,7 @@ module.exports = {
             SRC_DIR,
             NODE_MODULE_ROOT
         ],
-        extensions: ['.tsx', '.ts', '.js', '.json']
+        extensions: ['.tsx', '.ts', '.js', '.json', '.css', '.scss']
     },
     output: {
         path: DEST_DIR,
@@ -33,36 +32,7 @@ module.exports = {
         publicPath: '/app/'
     },
     module: {
-        // rules: [
-        //     {
-        //         test: /\.ts$/,
-        //         use: [
-        //             'awesome-typescript-loader',
-        //             'angular2-template-loader'
-        //         ]
-        //     },
-        //     {
-        //         test: /\.html$/,
-        //         exclude: /node_modules/,
-        //         use: 'html-loader'
-        //     }
-        // ],
         loaders: [
-            // {
-            //     test: /\.ts$/,
-            //     exclude: /node_modules/,
-            //     options: {configFileName: helpers.root('src', 'tsconfig.json')},
-            //     use: 'awesome-typescript-loader'
-            // }, {
-            //     test: /\.ts$/,
-            //     exclude: /node_modules/,
-            //     use: 'angular2-template-loader'
-            // },
-            // {
-            //     test: /\.html$/,
-            //     exclude: /node_modules/,
-            //     use: 'html-loader'
-            // }
             {
                 test: /\.ts$/,
                 use: [
@@ -74,7 +44,12 @@ module.exports = {
                 test: /\.html$/,
                 exclude: /node_modules/,
                 use: 'html-loader'
-            }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['to-string-loader', 'css-loader', 'sass-loader']
+            },
         ]
     },
     plugins:
